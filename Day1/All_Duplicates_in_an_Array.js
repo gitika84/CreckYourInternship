e5b814479bb0d49e19692251aecdb5d1a6e27be3
gitika84 <gitikasharma8j@gmai.com>
2024-11-11 23:03:29 +0530

    Another File

---
+++ Day1/All_Duplicates_in_an_Array.js
@@ -0,0 +1,20 @@
+function findDuplicates(nums) {
+    const result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        const index = Math.abs(nums[i]) - 1; 
+
+       
+        if (nums[index] < 0) {
+            result.push(index + 1); 
+        } else {
+            nums[index] = -nums[index];
+        }
+    }
+
+    return result;
+}
+
+const nums = [4,3,2,7,8,2,3,1];
+const duplicates = findDuplicates(nums);
+console.log(duplicates); 
