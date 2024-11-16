96ec6fbfa788773fd5b61319d382dab94cd2986f
gitika84 <gitikasharma8j@gmai.com>
2024-11-16 08:19:09 +0530

    Another Day

---
+++ Day6/Find_the_Index_of_the_First_Occurrence_in_a_String.js
@@ -0,0 +1,20 @@
+function strStr(haystack, needle) {
+    if (needle === "") return 0; 
+
+    const needleLength = needle.length;
+    const haystackLength = haystack.length;
+
+    for (let i = 0; i <= haystackLength - needleLength; i++) {
+        
+        if (haystack.slice(i, i + needleLength) === needle) {
+            return i;
+        }
+    }
+
+    return -1; 
+}
+
+console.log(strStr("hello", "ll"));
+console.log(strStr("aaaaa", "bba"));
+console.log(strStr("", ""));        
+console.log(strStr("abc", "c"));   
