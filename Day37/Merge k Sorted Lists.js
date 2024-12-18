1839538bc79d029173a1d413a310b3891a38854a
gitika84 <gitikasharma8j@gmai.com>
2024-12-18 23:11:00 +0530

    Another Day

---
+++ Day37/Merge k Sorted Lists.js
@@ -0,0 +1,35 @@
+function mergeLists(a, b) {
+    const dummy = new ListNode(0);
+    let temp = dummy;
+     while (a !== null && b !== null) {
+         if (a.val < b.val) {
+             temp.next = a;
+             a = a.next;
+         } else {
+             temp.next = b;
+             b = b.next;
+         }
+         temp = temp.next;
+     }
+    if (a !== null) {
+        temp.next = a;
+    }
+    if (b !== null) {
+        temp.next = b;
+    }
+    return dummy.next;
+}
+
+var mergeKLists = function(lists) {
+    if (lists.length === 0 ) {
+        return null;
+    }
+    
+    while (lists.length > 1) {
+        let a = lists.shift(); 
+        let b = lists.shift(); 
+        const h = mergeLists(a, b);
+        lists.push(h);
+    }
+    return lists[0];
+};
