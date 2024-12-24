98768e7f03671bd7fe01340ee020728b42b4e4b1
gitika84 <gitikasharma8j@gmai.com>
2024-12-24 23:38:14 +0530

    Another Day

---
+++ Day43/Minimum Deletions to Make Character Frequencies Unique.js
@@ -0,0 +1,21 @@
+
+var minDeletions = function(s) {
+    const chars = new Map();
+    for (const char of s) {
+        chars.set(char, (chars.get(char) || 0) + 1);
+    }
+
+    const freqSet = new Set();
+    let count = 0;
+
+    for (let freq of chars.values()) {
+        while (freq > 0 && freqSet.has(freq)) {
+            freq--;
+            count++;
+        }
+
+      freqSet.add(freq);
+    }
+
+    return count;    
+};
